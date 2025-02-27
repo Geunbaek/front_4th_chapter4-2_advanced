b6b88ffac44e0357236ac16c64377fb9a7c53d95
Geunbaek <rmsqor103@naver.com>
2025-02-28 04:44:43 +0900

    refactor: dnd provider 위치 변경

--- src/schedule/ui/ScheduleTable.tsx
+++ src/schedule/ui/ScheduleTable.tsx
@@ -46,12 +46,17 @@ const getColor = (schedules: Schedule[], lectureId: string): string => {
 };
 
 const ScheduleTableWrapper = ({ tableId, children }: PropsWithChildren<{ tableId: string }>) => {
-  const { activeTableId, setActiveTableId } = useScheduleStore(
-    useShallow(state => ({
-      activeTableId: state.activeTableId,
-      setActiveTableId: state.setActiveTableId,
-    })),
+  const activeTableId = useScheduleStore(state => state.activeTableId);
+
+  return (
+    <Box position="relative" outline={activeTableId === tableId ? '5px dashed' : undefined} outlineColor="blue.300">
+      {children}
+    </Box>
   );
+};
+
+const ScheduleDndMonitor = () => {
+  const setActiveTableId = useScheduleStore(state => state.setActiveTableId);
 
   useDndMonitor({
     onDragStart({ active }) {
@@ -66,11 +71,7 @@ const ScheduleTableWrapper = ({ tableId, children }: PropsWithChildren<{ tableId
     },
   });
 
-  return (
-    <Box position="relative" outline={activeTableId === tableId ? '5px dashed' : undefined} outlineColor="blue.300">
-      {children}
-    </Box>
-  );
+  return null;
 };
 
 const ScheduleTable = ({ tableId }: Props) => {
@@ -89,55 +90,53 @@ const ScheduleTable = ({ tableId }: Props) => {
   };
 
   return (
-    <ScheduleDndProvider tableId={tableId}>
-      <ScheduleTableWrapper tableId={tableId}>
-        <Grid
-          templateColumns={`120px repeat(${DAY_LABELS.length}, ${CellSize.WIDTH}px)`}
-          templateRows={`40px repeat(${TIMES.length}, ${CellSize.HEIGHT}px)`}
-          bg="white"
-          fontSize="sm"
-          textAlign="center"
-          outline="1px solid"
-          outlineColor="gray.300"
-        >
-          <GridItem key="교시" borderColor="gray.300" bg="gray.100">
-            <Flex justifyContent="center" alignItems="center" h="full" w="full">
-              <Text fontWeight="bold">교시</Text>
+    <ScheduleTableWrapper tableId={tableId}>
+      <Grid
+        templateColumns={`120px repeat(${DAY_LABELS.length}, ${CellSize.WIDTH}px)`}
+        templateRows={`40px repeat(${TIMES.length}, ${CellSize.HEIGHT}px)`}
+        bg="white"
+        fontSize="sm"
+        textAlign="center"
+        outline="1px solid"
+        outlineColor="gray.300"
+      >
+        <GridItem key="교시" borderColor="gray.300" bg="gray.100">
+          <Flex justifyContent="center" alignItems="center" h="full" w="full">
+            <Text fontWeight="bold">교시</Text>
+          </Flex>
+        </GridItem>
+        {DAY_LABELS.map(day => (
+          <GridItem key={day} borderLeft="1px" borderColor="gray.300" bg="gray.100">
+            <Flex justifyContent="center" alignItems="center" h="full">
+              <Text fontWeight="bold">{day}</Text>
             </Flex>
           </GridItem>
-          {DAY_LABELS.map(day => (
-            <GridItem key={day} borderLeft="1px" borderColor="gray.300" bg="gray.100">
+        ))}
+        {TIMES.map((time, timeIndex) => (
+          <Fragment key={`시간-${timeIndex + 1}`}>
+            <GridItem borderTop="1px solid" borderColor="gray.300" bg={timeIndex > 17 ? 'gray.200' : 'gray.100'}>
               <Flex justifyContent="center" alignItems="center" h="full">
-                <Text fontWeight="bold">{day}</Text>
+                <Text fontSize="xs">
+                  {fill2(timeIndex + 1)} ({time})
+                </Text>
               </Flex>
             </GridItem>
-          ))}
-          {TIMES.map((time, timeIndex) => (
-            <Fragment key={`시간-${timeIndex + 1}`}>
-              <GridItem borderTop="1px solid" borderColor="gray.300" bg={timeIndex > 17 ? 'gray.200' : 'gray.100'}>
-                <Flex justifyContent="center" alignItems="center" h="full">
-                  <Text fontSize="xs">
-                    {fill2(timeIndex + 1)} ({time})
-                  </Text>
-                </Flex>
-              </GridItem>
-              {DAY_LABELS.map(day => (
-                <GridItem
-                  key={`${day}-${timeIndex + 2}`}
-                  borderWidth="1px 0 0 1px"
-                  borderColor="gray.300"
-                  bg={timeIndex > 17 ? 'gray.100' : 'white'}
-                  cursor="pointer"
-                  _hover={{ bg: 'yellow.100' }}
-                  onClick={() => handleScheduleTimeClick(tableId, day, timeIndex + 1)}
-                />
-              ))}
-            </Fragment>
-          ))}
-        </Grid>
-        <DraggableScheduleList tableId={tableId} />
-      </ScheduleTableWrapper>
-    </ScheduleDndProvider>
+            {DAY_LABELS.map(day => (
+              <GridItem
+                key={`${day}-${timeIndex + 2}`}
+                borderWidth="1px 0 0 1px"
+                borderColor="gray.300"
+                bg={timeIndex > 17 ? 'gray.100' : 'white'}
+                cursor="pointer"
+                _hover={{ bg: 'yellow.100' }}
+                onClick={() => handleScheduleTimeClick(tableId, day, timeIndex + 1)}
+              />
+            ))}
+          </Fragment>
+        ))}
+      </Grid>
+      <DraggableScheduleList tableId={tableId} />
+    </ScheduleTableWrapper>
   );
 };
 
@@ -146,12 +145,10 @@ const DraggableScheduleList = memo(({ tableId }: Props) => {
   return (
     <>
       {schedules.map((schedule, index) => (
-        <DraggableSchedule
-          key={`${schedule.lecture.title}-${index}`}
-          id={`${tableId}:${index}`}
-          data={schedule}
-          bg={getColor(schedules, schedule.lecture.id)}
-        />
+        <ScheduleDndProvider key={`${schedule.lecture.title}-${index}`} tableId={tableId}>
+          <ScheduleDndMonitor />
+          <DraggableSchedule id={`${tableId}:${index}`} data={schedule} bg={getColor(schedules, schedule.lecture.id)} />
+        </ScheduleDndProvider>
       ))}
     </>
   );
